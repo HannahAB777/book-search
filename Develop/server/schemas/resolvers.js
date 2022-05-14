5eb63e9892c7183a2e7f84056777ae65442fd2d5
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-14 16:37:54 +0800

    login mutation done

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -25,20 +25,67 @@ const resolvers = {
 
 
   Mutation: {
-    login: async (parent, args, context) =>{
+    login: async (parent, {email, password}) =>{
+      
+        const user = await User.findOne({ email: email});
+        
+        if (!user) {
+          throw new Error("No user found matching that email");
+        }
+    
+        const correctPw = await user.isCorrectPassword(password);
+    
+        if (!correctPw) {
+          throw new Error("password is incorrect");
+        }
 
+        const token = signToken(user);
+        
+        return { token, user },
+      
     }
 
     addUser: async (parent, args, context) =>{
-
+      async createUser({ body }, res) {
+        const user = await User.create(body);
+    
+        if (!user) {
+          return res.status(400).json({ message: 'Something is wrong!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+      },
     },
 
     saveBook: async (parent, args, context) =>{
-
+      async saveBook({ user, body }, res) {
+        console.log(user);
+        try {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $addToSet: { savedBooks: body } },
+            { new: true, runValidators: true }
+          );
+          return res.json(updatedUser);
+        } catch (err) {
+          console.log(err);
+          return res.status(400).json(err);
+        }
+      },
     },
 
     removeBook: async (parent, args, context) =>{
-
+      async deleteBook({ user, params }, res) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId: params.bookId } } },
+          { new: true }
+        );
+        if (!updatedUser) {
+          return res.status(404).json({ message: "Couldn't find user with this id!" });
+        }
+        return res.json(updatedUser);
+      },
     },
 
   
