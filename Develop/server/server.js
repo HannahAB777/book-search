614ea5d4045ee73953c1d8d607b06ecc0970c0eb
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-21 09:06:27 +0800

    push again

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -24,7 +24,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
